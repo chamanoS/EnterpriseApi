38544aaa280d308228342326927f4cc79c0fedcd
Chamano Ramashia <chamano.sr@gmail.com>
2026-01-07 09:34:19 +0200

    adding patient model, getters and setters

---
+++ Models/Patient.cs
@@ -0,0 +1,8 @@
+namespace HelloEnterpriseApi.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
